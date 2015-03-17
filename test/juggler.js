2997943e48a6178be1fbff8c1f2d82d48c308e4d
parhelium <me@parhelium.pl>
2015-03-17 12:02:21 +0100

    tests improvements : added more cases, fixed broken tests

--- test/juggler.js
+++ test/juggler.js
@@ -1,16 +1,29 @@
 var should = require( 'chai' ).should(),
     parser = require( '../parseExportedEmails' ),
-    fs = require('fs'),
+    fs = require( 'fs' ),
     juggler = require( '../juggler' ).run;
 
 describe( 'base functionality', function () {
-    it( '1 source without blacklists', function () {
-        juggler(['./test/source1.csv'],null).should.equal(fs.readFileSync('./test/result_s.csv' ).toString());
+    it( 'sources1 : only one source without blacklist ', function () {
+        juggler( ['./test/source1.csv'], null ).should.equal( fs.readFileSync( './test/result_s.csv' ).toString() );
     } );
-    it( '2 sources with duplicates', function () {
-        juggler(['./test/source1.csv', './test/source2.csv'],null).should.equal(fs.readFileSync('./test/result_s.csv' ).toString());
+    it( 'sources11 : source duplicated', function () {
+        juggler( ['./test/source1.csv', './test/source1duplicate.csv'], null ).should.equal( fs.readFileSync( './test/result_s.csv' ).toString() );
     } );
-    it( '2 sources with blacklists', function () {
-        juggler(['./test/source1.csv', './test/source2.csv'],['./test/blacklist1.csv']).should.equal(fs.readFileSync('./test/result_s_minus_b.csv' ).toString());
+    it( 'sources1 - blacklists1 : one source without one blacklist', function () {
+        juggler(
+            ['./test/source1.csv'],
+            ['./test/blacklist1.csv']
+        ).should.equal(
+            fs.readFileSync( './test/result_s1_minus_b1.csv' ).toString()
+        );
+    } );
+    it( 'sources12 - blacklists12 : two sources without two blacklists', function () {
+        juggler(
+            ['./test/source1.csv', './test/source2.csv'],
+            ['./test/blacklist1.csv', './test/blacklist2.csv']
+        ).should.equal(
+            fs.readFileSync( './test/result_s12_minus_b12.csv' ).toString()
+        );
     } );
 } )
