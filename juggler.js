9838e9ccaceba09b5fad17c87ee6f18da9996000
parhelium <me@parhelium.pl>
2015-03-17 10:59:19 +0100

    code clean

--- juggler.js
+++ juggler.js
@@ -1,24 +1,13 @@
 #!/usr/bin/env node
 
-/**
- * Module dependencies.
- */
-
-
 var is = require( 'is_js' );
 var parser = require( './parseExportedEmails' );
 var program = require( 'commander' );
 var _ = require( 'lodash' );
 
-// var ts      = "the-shopist.csv";
-// var we      = "welonik.csv";
-// var tsFile = fs.readFileSync(ts).toString();
-
-
 function parseList( val ) {
     return val.split( ',' );
 }
-
 
 program
     .version( '0.0.1' )
@@ -38,12 +27,13 @@ var emails = [];
 
 
 uniqGoodEmails = parser.mergeFiles( program.sources );
-console.log( 'BEFORE : uniqGoodEmails.length = ', uniqGoodEmails.length )
+
+console.log( 'sources amount of emails :  ', uniqGoodEmails.length )
 emails = uniqGoodEmails;
 
 if ( program.diff && is.array( program.diff ) ) {
     uniqBadEmail = parser.mergeFiles( program.diff );
     emails = _.difference( uniqGoodEmails, uniqBadEmail )
 }
-console.log( 'uniqBadEmail.length ', uniqBadEmail.length )
-console.log( 'AFTER : uniqGoodEmails.length ', uniqGoodEmails.length )
+console.log( 'emails to remove amount : ', uniqBadEmail.length )
+console.log( 'sources - diff emails amount : ', uniqGoodEmails.length )
